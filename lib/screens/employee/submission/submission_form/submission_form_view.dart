649eea6ebabc25a29f07e9fb73869ecf6f81a721
Yudha <yudhah52@gmail.com>
2024-09-21 17:33:12 +0700

    show modal on willpopscope reimburse form

--- lib/screens/employee/submission/submission_form/submission_form_view.dart
+++ lib/screens/employee/submission/submission_form/submission_form_view.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:reimburse_rb/provider/user_provider.dart';
 import 'package:reimburse_rb/screens/employee/submission/submission_form/submission_form_view_model.dart';
 import 'package:reimburse_rb/utility/constant.dart';
+import 'package:reimburse_rb/utility/helper.dart';
 import 'package:reimburse_rb/widgets/common/appbar_general.dart';
 import 'package:reimburse_rb/widgets/common/button_general.dart';
 import 'package:reimburse_rb/widgets/common/form_dropdown_purpose.dart';
@@ -52,138 +53,156 @@ class _SubmissionFormViewState extends State<SubmissionFormView> with TickerProv
   Widget build(BuildContext context) {
     final viewModel = context.watch<SubmissionFormViewModel>();
     final userProvider = context.read<UserProvider>();
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBarGeneral(
-        context: context,
-        title: 'Reimburse ${userProvider.selectedReimbursementCategory?.categoryReimbursementText}',
-      ),
-      body: LoadingFallback(
-        isLoading: viewModel.isLoading,
-        child: ListView(
-          children: [
-            const SizedBox(height: 24),
-            Form(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    FormFieldText(
-                      controllerName: viewModel.nameController,
-                      placeholder: "Nama Karyawan",
-                      hintText: "Masukkan Nama Karyawan",
-                      isEnabled: false,
-                    ),
-                    const SizedBox(height: 16),
-                    FormDropdownPurpose(
-                      hintText:
-                          userProvider.selectedReimbursementCategory?.categoryReimbursementId ==
-                                  Constant.healthCategoryReimbursementId
-                              ? 'Pilih Diagnosis'
-                              : 'Pilih Tujuan',
-                      value: viewModel.selectedPurpose,
-                      items: viewModel.listPurposeOption ?? [],
-                      placeholder:
-                          userProvider.selectedReimbursementCategory?.categoryReimbursementId ==
-                                  Constant.healthCategoryReimbursementId
-                              ? 'Diagnosis'
-                              : 'Tujuan',
-                      onChanged: (newValue) {
-                        viewModel.onChangePurpose(newSelectedPurpose: newValue);
-                      },
-                    ),
-                    if (viewModel.selectedPurpose?.purpose_id == 1) ...[
-                      const SizedBox(height: 8),
+    return WillPopScope(
+      onWillPop: () async {
+        await Helper(context: context).alertClose(
+          title: 'Konfirmasi',
+          message: 'Apakah Anda yakin akan keluar dari halaman ini? Perubahan tidak akan disimpan.',
+          context: context,
+          firstButtonOnTap: () {
+            Navigator.pop(context);
+            Navigator.pop(context);
+          },
+          secondButtonOnTap: () {
+            Navigator.pop(context);
+          },
+        );
+        return false; // return false if alertClose is shown
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBarGeneral(
+          context: context,
+          title:
+              'Reimburse ${userProvider.selectedReimbursementCategory?.categoryReimbursementText}',
+        ),
+        body: LoadingFallback(
+          isLoading: viewModel.isLoading,
+          child: ListView(
+            children: [
+              const SizedBox(height: 24),
+              Form(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
                       FormFieldText(
-                        controllerName: viewModel.otherPurposeController,
+                        controllerName: viewModel.nameController,
+                        placeholder: "Nama Karyawan",
+                        hintText: "Masukkan Nama Karyawan",
+                        isEnabled: false,
+                      ),
+                      const SizedBox(height: 16),
+                      FormDropdownPurpose(
                         hintText:
                             userProvider.selectedReimbursementCategory?.categoryReimbursementId ==
                                     Constant.healthCategoryReimbursementId
-                                ? 'Masukkan Diagnosis Lain'
-                                : 'Masukkan Tujuan Lain',
-                        // note: userProvider.selectedReimbursementCategory?.categoryReimbursementId ==
-                        //         Constant.healthCategoryReimbursementId
-                        //     ? Constant.noteFormPurposeHealth
-                        //     : Constant.noteFormPurposeTransport,
-                        // prefixIconNote: Icon(
-                        //   Icons.info_rounded,
-                        //   color: Constant.waitingStatusColor,
-                        // ),
+                                ? 'Pilih Diagnosis'
+                                : 'Pilih Tujuan',
+                        value: viewModel.selectedPurpose,
+                        items: viewModel.listPurposeOption ?? [],
+                        placeholder:
+                            userProvider.selectedReimbursementCategory?.categoryReimbursementId ==
+                                    Constant.healthCategoryReimbursementId
+                                ? 'Diagnosis'
+                                : 'Tujuan',
+                        onChanged: (newValue) {
+                          viewModel.onChangePurpose(newSelectedPurpose: newValue);
+                        },
                       ),
+                      if (viewModel.selectedPurpose?.purpose_id == 1) ...[
+                        const SizedBox(height: 8),
+                        FormFieldText(
+                          controllerName: viewModel.otherPurposeController,
+                          hintText:
+                              userProvider.selectedReimbursementCategory?.categoryReimbursementId ==
+                                      Constant.healthCategoryReimbursementId
+                                  ? 'Masukkan Diagnosis Lain'
+                                  : 'Masukkan Tujuan Lain',
+                          // note: userProvider.selectedReimbursementCategory?.categoryReimbursementId ==
+                          //         Constant.healthCategoryReimbursementId
+                          //     ? Constant.noteFormPurposeHealth
+                          //     : Constant.noteFormPurposeTransport,
+                          // prefixIconNote: Icon(
+                          //   Icons.info_rounded,
+                          //   color: Constant.waitingStatusColor,
+                          // ),
+                        ),
+                      ],
+                      const SizedBox(height: 16),
+                      FormFieldText(
+                        placeholder: "Total Biaya",
+                        isEnabled: false,
+                        hintText: "Total Biaya Otomatis",
+                        keyboardType: TextInputType.number,
+                        controllerName: viewModel.totalCostController,
+                        isCost: true,
+                        note: Constant.noteFormTotalCost,
+                        prefixIconNote: Icon(
+                          Icons.info_rounded,
+                          color: Constant.waitingStatusColor,
+                        ),
+                        noteStyle: Constant.regularNoteStyle,
+                      ),
+                      const SizedBox(height: 16),
                     ],
-                    const SizedBox(height: 16),
-                    FormFieldText(
-                      placeholder: "Total Biaya",
-                      isEnabled: false,
-                      hintText: "Total Biaya Otomatis",
-                      keyboardType: TextInputType.number,
-                      controllerName: viewModel.totalCostController,
-                      isCost: true,
-                      note: Constant.noteFormTotalCost,
-                      prefixIconNote: Icon(
-                        Icons.info_rounded,
-                        color: Constant.waitingStatusColor,
-                      ),
-                      noteStyle: Constant.regularNoteStyle,
-                    ),
-                    const SizedBox(height: 16),
-                  ],
-                ),
-              ),
-            ),
-            FormImageAttachment(
-              title: 'Lampiran Bukti',
-              viewModel: viewModel,
-              onTapAddImage: () {
-                viewModel.onTapAddImage(animationController: animationController);
-              },
-            ),
-            const SizedBox(height: 24),
-            ListView.builder(
-              itemCount: viewModel.listBodyDetailCost.length,
-              shrinkWrap: true,
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: const EdgeInsets.only(bottom: 16),
-                  child: CardFormDetailCost(
-                    viewModel: viewModel,
-                    detailCostIndex: index,
                   ),
-                );
-              },
-            ),
-            const SizedBox(height: 8),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: ButtonGeneral(
-                onTap: () {
-                  viewModel.addDetailCost();
-                },
-                prefixIcon: const Icon(
-                  Icons.add_rounded,
-                  color: Constant.green,
                 ),
-                isWhiteButton: true,
-                color: Constant.greenDark,
-                text: 'Tambahkan Rincian',
               ),
-            ),
-            const SizedBox(height: 48),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: ButtonGeneral(
-                onTap: () {
-                  viewModel.postUploadSubmission();
+              FormImageAttachment(
+                title: 'Lampiran Bukti',
+                viewModel: viewModel,
+                onTapAddImage: () {
+                  viewModel.onTapAddImage(animationController: animationController);
                 },
-                color: Constant.greenDark,
-                text: 'Unggah Pengajuan',
               ),
-            ),
-            const SizedBox(height: 72),
-          ],
+              const SizedBox(height: 24),
+              ListView.builder(
+                itemCount: viewModel.listBodyDetailCost.length,
+                shrinkWrap: true,
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 16),
+                    child: CardFormDetailCost(
+                      viewModel: viewModel,
+                      detailCostIndex: index,
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(height: 8),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: ButtonGeneral(
+                  onTap: () {
+                    viewModel.addDetailCost();
+                  },
+                  prefixIcon: const Icon(
+                    Icons.add_rounded,
+                    color: Constant.green,
+                  ),
+                  isWhiteButton: true,
+                  color: Constant.greenDark,
+                  text: 'Tambahkan Rincian',
+                ),
+              ),
+              const SizedBox(height: 48),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: ButtonGeneral(
+                  onTap: () {
+                    viewModel.postUploadSubmission();
+                  },
+                  color: Constant.greenDark,
+                  text: 'Unggah Pengajuan',
+                ),
+              ),
+              const SizedBox(height: 72),
+            ],
+          ),
         ),
       ),
     );
